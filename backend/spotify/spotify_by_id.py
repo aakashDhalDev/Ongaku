be82264b6b9721e856f645a40c1c04b2777fd147
Aakash Dhal <134286523+aakashDhalDev@users.noreply.github.com>
2024-09-05 19:49:30 -0700

    Update spotify_by_id.py

--- backend/spotify/spotify_by_id.py
+++ backend/spotify/spotify_by_id.py
@@ -51,33 +51,3 @@ def get_tracks_info(track_ids):
         return response.json()["tracks"]
     else:
         raise Exception(f"Error: {response.status_code}, {response.text}")
-
-# # Replace these with your actual Client ID and Client Secret
-
-# # List of track IDs (replace with your actual track IDs)
-# track_ids = [
-#     '3n3Ppam7vgaVa1iaRUc9Lp',
-#     '0eGsygTp906u18L0Oimnem',
-#     '1dGr1c8CrMLDpV6mPbImSI',
-#     # Add more track IDs as needed
-# ]
-
-# # Get track information
-# tracks_info = get_tracks_info(access_token, track_ids)
-
-# # Print track details
-# for track in tracks_info:
-#     print(f"ID: {track['id']}")
-#     print(f"Name: {track['name']}")
-#     print(f"Artists: {', '.join(artist['name'] for artist in track['artists'])}")
-#     print(f"Album: {track['album']['name']}")
-#     print(f"Release Date: {track['album']['release_date']}")
-#     print(f"Popularity: {track['popularity']}")
-#     print('-----------------------------')
-
-# # Save the track details to a JSON file
-# with open('track_details.json', 'w') as f:
-#     json.dump(tracks_info, f, indent=4)
-
-# print("Track details saved to 'track_details.json'.")
-
